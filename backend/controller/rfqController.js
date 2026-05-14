d955741f4c052b9c38242142054aea3a58c00bf8
Sajindu Jinasena <Sajindu.Jinasena@groupit.hayleys.com>
2026-05-14 22:16:21 +0530

    Project setup

---
+++ backend/controller/rfqController.js
@@ -0,0 +1,47 @@
+const RFQ = require("../model/RFQModel");
+
+const createRFQ = async (req, res) => {
+  try {
+    const {
+      companyName,
+      contactPerson,
+      businessEmail,
+      phoneNumber,
+      projectName,
+      description,
+    } = req.body;
+
+    const uploadedFiles = req.files
+      ? req.files.map((file) => file.filename)
+      : [];
+
+    const newRFQ = new RFQ({
+      companyName,
+      contactPerson,
+      businessEmail,
+      phoneNumber,
+      projectName,
+      description,
+      files: uploadedFiles,
+    });
+
+    await newRFQ.save();
+
+    res.status(201).json({
+      success: true,
+      message: "RFQ Submitted Successfully",
+      data: newRFQ,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
+
+module.exports = {
+  createRFQ,
+};
